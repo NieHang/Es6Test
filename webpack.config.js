b1575fed728ccdf24791265c536e533b7f846488
Bulb <1076883711@qq.com>
2018-07-08 10:59:13 +0800

    Es6模块化

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ module.exports = {
     devtool: 'eval-source-map',
     entry: __dirname + "/modules/main.js", //已多次提及的唯一入口文件
     output: {
-        path: __dirname + "/public", //打包后的文件存放的地方
+        path: __dirname + "/dist", //打包后的文件存放的地方
         filename: "bundle.js" //打包后输出文件的文件名
     },
     module: {
@@ -16,7 +16,7 @@ module.exports = {
     },
 
     devServer: {
-        contentBase: "./public", //本地服务器所加载的页面所在的目录
+        contentBase: "./dist", //本地服务器所加载的页面所在的目录
         historyApiFallback: true, //不跳转
         inline: true //实时刷新
     }
